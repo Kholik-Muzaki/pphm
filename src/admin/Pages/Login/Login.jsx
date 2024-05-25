3381b55d3c7637b896086c01966205bf7b1c254a
Kholik Muzaki <98199875+Kholik-Muzaki@users.noreply.github.com>
2024-05-25 14:23:06 +0700

    update: validation form login

--- src/admin/Pages/Login/Login.jsx
+++ src/admin/Pages/Login/Login.jsx
@@ -1,7 +1,49 @@
+import { useNavigate } from "react-router-dom"
 import image from "../../../Image"
 import './Login.style.css'
-
+import { useFormik } from "formik"
+import * as Yup from "yup"
 const Login = () => {
+
+    const navigate = useNavigate();
+    const handleSubmit = (values) => {
+        // alert("Login Success");
+        // alert(formik.values.role)
+        // alert(formik.values.username)
+        // alert(formik.values.password)
+        // navigate("/admin");
+
+        const { username, password, role } = values;
+        if (username === "admin" && password === "admin" && role === "Admin") {
+            navigate("/admin");
+            alert("Login Success as Admin");
+        } else if (username === "bendahara" && password === "bendahara" && role === "Bendahara") {
+            navigate("/bendahara");
+            alert("Login Success as Bendahar");
+        } else (
+            alert("Invalid credentials")
+        )
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: "",
+            role: "",
+        },
+        onSubmit: handleSubmit,
+        validationSchema: Yup.object().shape({
+            username: Yup.string().required("Username harus diisi!"),
+            password: Yup.string().required("Password harus diisi!"),
+            role: Yup.string().required("Role harus diisi!"),
+        })
+    })
+
+    const handleChange = (event) => {
+        const { target } = event
+        formik.setFieldValue(target.name, target.value)
+    }
+
     return (
         <>
             <div className="container-fluid login-container">
@@ -14,20 +56,22 @@ const Login = () => {
                         <h2 className="selamat-datang">Selamat Datang Kembali</h2>
                         <p>Silahkan melakukan login dengan mengisi form di bawah!</p>
 
-                        <form action="">
+                        <form onSubmit={formik.handleSubmit}>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputEmail1" className="form-label">
                                     Username
                                 </label>
                                 <input
                                     type="text"
-                                    className="form-control"
+                                    className={`form-control ${formik.touched.username && formik.errors.username ? "is-invalid" : ""}`}
                                     id="username"
                                     aria-describedby="username"
+                                    name="username"
+                                    onChange={handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.username}
                                 />
-                                <div id="username" className="form-text">
-                                    Silahkan mengisi username anda
-                                </div>
+                                {formik.touched.username && formik.errors.username && <div className="invalid-feedback">{formik.errors.username}</div>}
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputPassword1" className="form-label">
@@ -35,18 +79,31 @@ const Login = () => {
                                 </label>
                                 <input
                                     type="password"
-                                    className="form-control"
+                                    className={`form-control ${formik.touched.password && formik.errors.password ? "is-invalid" : ""}`}
                                     id="exampleInputPassword1"
+                                    name="password"
+                                    onChange={handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
                                 />
+                                {formik.touched.password && formik.errors.password && <div className="invalid-feedback">{formik.errors.password}</div>}
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="role" className="form-label">
                                     Role
                                 </label>
-                                <select id="role" className="form-select">
-                                    <option>Admin</option>
-                                    <option>Bendahara</option>
+                                <select
+                                    id="role"
+                                    className={`form-select ${formik.touched.role && formik.errors.role ? "is-invalid" : ""}`}
+                                    name="role"
+                                    onChange={handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.role}>
+                                    <option value="" label="Pilih Role" />
+                                    <option value="Admin" label="Admin" />
+                                    <option value="Bendahara" label="Bendahara" />
                                 </select>
+                                {formik.touched.role && formik.errors.role && <div className="invalid-feedback">{formik.errors.role}</div>}
                             </div>
 
                             <button type="submit" className="btn btn-primary">
