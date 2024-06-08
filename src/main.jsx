a35fd411e1144a5ccffe852c91d2d3d0480f98ed
Kholik Muzaki <98199875+Kholik-Muzaki@users.noreply.github.com>
2024-06-08 19:59:33 +0700

    add: library primeReact, PrimeFlex

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'boxicons/css/boxicons.min.css';
-
-
+// Impor CSS PrimeReact
+import 'primereact/resources/themes/saga-blue/theme.css';  // Pilih tema yang diinginkan
+import 'primereact/resources/primereact.min.css';          // Gaya utama PrimeReact
+import 'primeicons/primeicons.css';
+import 'primeflex/themes/primeone-light.css'   
+import 'primeflex/themes/primeone-dark.css'  
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
